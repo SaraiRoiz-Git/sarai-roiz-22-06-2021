de8199e8a7eab445455b0c57d01711a40fc517ae
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-06-27 12:30:09 +0300

    key

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'JpdkGpxHZZap1ivLtsaedaEW3GCQ8yrL';
-//export const API_KEY = 'Ba9VuPRBgWhHB7J4wQhtnAai5sosoTOP';
+//export const API_KEY = 'JpdkGpxHZZap1ivLtsaedaEW3GCQ8yrL';
+export const API_KEY = 'Ba9VuPRBgWhHB7J4wQhtnAai5sosoTOP';
 export const URL = 'https://dataservice.accuweather.com/';
 export const LOCAL_PLAECE = { "Version": 1, "Key": "215854", "Type": "City", "Rank": 31, "LocalizedName": "Tel Aviv", "Country": { "ID": "IL", "LocalizedName": "Israel" }, "AdministrativeArea": { "ID": "TA", "LocalizedName": "Tel Aviv" } }
